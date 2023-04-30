aa61b0ad0a59701cd1b8303c4ad491722ffe814f
Liudmila <lidiay@yandex.ru>
2023-04-30 21:54:07 +0300

    Домашка к уроку 5 ООП add

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 package HomeworksAdd.HWProjectOOP;
 
+import HomeworksAdd.HWProjectOOP.view.ConsoleView;
+import HomeworksAdd.HWProjectOOP.view.View;
+
 public class Main {
     public static void main(String[] args) {
-        
+        View view = new ConsoleView();
+        view.start();
     }
 }
